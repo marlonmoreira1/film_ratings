814d69bc99e468daadcbde738971b3bec55154d7
marlonmoreira1 <71144665+marlonmoreira1@users.noreply.github.com>
2025-10-22 19:48:13 -0300

    Fix com content

--- app/app.py
+++ app/app.py
@@ -213,7 +213,7 @@ for i, filme in enumerate(filmes_pagina):
         st.write(f"##### - **Nota:** {nota}")
         st.write(f"##### - {extra_info}")
         if poster:
-            st.image(poster, caption=nome, width="stretch")
+            st.image(poster, caption=nome, width="content")
         else:
             st.write("_Sem imagem disponível_")
 
